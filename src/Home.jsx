9966822877d70f3027f335b809edcb27b84c08f5
brettl <lukas.brettl@seznam.cz>
2025-04-13 16:22:07 +0200

    add iframe

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,14 @@
-
+import { Link } from 'react-router-dom';
 export default function Home() {
 
 
   return (
-      <h1 className="text-4xl">Zatím tu nic není :)</h1>
+    <div className="flex flex-col items-center">
+      <h1 className="text-4xl mb-20">Zatím tu nic není :)</h1>
+      <Link to="/hory2">
+        <button className="cursor-pointer text-center bg-slate-800 p-3 rounded-md ">Nové video: Hory2 🏔️</button>
+      </Link>
+    </div>
   )
 }
 
--- src/Hory2.jsx
+++ src/Hory2.jsx
@@ -1,10 +1,14 @@
-
 export default function Hory2() {
-
-
     return (
-        <h1 className="text-4xl">soon</h1>
-    )
+      <div className="w-full h-full flex items-center justify-center">
+          <iframe
+            src="https://drive.google.com/file/d/1E5fGb1fcdUMx1dWraN7K5z_VjolvTDvG/preview"
+            className="w-full h-full"
+            allow="autoplay"
+            allowFullScreen
+          ></iframe>
+      </div>
+    );
   }
   
   
