1ce8d510b7bba1d3f8a79aa2b7ee8dcd1a5cd86d
brettl <lukas.brettl@seznam.cz>
2025-04-13 16:09:28 +0200

    fix router 1

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,18 @@
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 import './index.css'
 import Home from './Home.jsx'
 import Hory2 from "./Hory2.jsx"
 
-
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
 root.render(
-  <BrowserRouter basename="/videos4"> 
+  <HashRouter>
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/hory2" element={<Hory2/>} />
-
+      <Route path="/hory2" element={<Hory2 />} />
     </Routes>
-  </BrowserRouter>
+  </HashRouter>
 );
