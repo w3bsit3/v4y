a9f92edaca73a3c516d2337182e3c0d81d1691a9
brettl <lukas.brettl@seznam.cz>
2025-04-27 14:30:02 +0200

    other

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,12 +1,13 @@
 import { Link } from 'react-router-dom';
 export default function Home() {
 
+const path_video1 = process.env.PATH_VIDEO1;
 
   return (
     <div className="flex flex-col items-center">
       <h1 className="text-4xl mb-20">Zatím tu nic není :)</h1>
-      <Link to="/hory2">
-        <button className="cursor-pointer text-center bg-slate-800 p-3 rounded-md ">Nové video: Hory2 🏔️</button>
+      <Link to={path_video1}>
+        <button className="cursor-pointer text-center bg-slate-800 p-3 rounded-md ">Nové video</button>
       </Link>
     </div>
   )
